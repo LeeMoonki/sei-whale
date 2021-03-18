deca410ba43d5295cee129e1876b009524bd781b
Junep <prepared@inflearn.com>
2021-03-18 11:24:45 +0900

    fix: docker-compose에서 port binding 이슈 해결

--- services/server/server.ts
+++ services/server/server.ts
@@ -7,7 +7,7 @@ const PORT =
   || 3000;
 
 function start(port: number | string) {
-  fastify.listen(port, function(err, address) {
+  fastify.listen(port, '0.0.0.0', function(err, address) {
     if (err) {
       fastify.log.error(err.message);
       process.exit(1);
