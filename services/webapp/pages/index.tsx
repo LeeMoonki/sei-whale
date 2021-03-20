2bd1e11974d79ab70305fb2bb962458b1d6df2eb
Junep <prepared@inflearn.com>
2021-03-20 09:11:50 +0900

    new: static page 예제 작성 및 webapp 포트 변경

--- services/webapp/pages/index.tsx
+++ services/webapp/pages/index.tsx
@@ -1,18 +1,25 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { useState, useEffect } from 'react';
 
 export default function Home() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/').then(r => r.json()).then(res => {
+      console.log(res);
+    });
+  }, []);
+
   return (
     <div>
       <Head>
         <title>sei-whale</title>
       </Head>
       <h1>Sei Whale Service!!</h1>
-      <button onClick={() => {
-        fetch('http://localhost:3000/').then(r => r.json()).then(res => {
-          console.log(res);
-        });
-      }}>click</button>
+      <div>{count}</div>
+      <button type="button" onClick={() => {
+        setCount(count + 1);
+      }}>증가</button>
     </div>
   )
 }
---
+++ services/webapp/pages/lists/[id].tsx
@@ -0,0 +1,40 @@
+import Head from 'next/head'
+
+interface Props {
+  posts: number[];
+}
+
+export default function Home({ posts }: Props) {
+  return (
+    <div>
+      <Head>
+        <title>sei-whale</title>
+      </Head>
+      <h1>Sei Whale list</h1>
+      {posts && posts.map((n, idx) => (<div key={idx}>{n}</div>))}
+    </div>
+  )
+}
+
+interface Path {
+  params: {
+    id?: string;
+  };
+}
+
+export async function getStaticPaths() {
+  const paths: Path[] = [
+    { params: { id: '1' } },
+    { params: { id: '2' } },
+  ];
+  return { paths, fallback: false }; // fallback은 다른 라우트에 대해선 404를 반환하도록 설정한다.
+}
+
+export async function getStaticProps({ params }: Path): Promise<{ props: Props; }> {
+  console.log('params : ', params);
+
+  // params를 사용해 data를 fetch 한다.
+  const data = [10, 20].map(n => n + +(params.id || 10000)); // parmas를 사용해 가져온 데이터라고 가정
+
+  return { props: { posts: data } };
+}
