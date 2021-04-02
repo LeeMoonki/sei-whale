02d5a1d4ea89c3c4a860516c19c83693e45588dd
Moonki Lee <shinbaek89@gmail.com>
2021-04-02 22:08:54 +0900

    new: prettier, eslint, sass 설정

--- services/webapp/pages/index.tsx
+++ services/webapp/pages/index.tsx
@@ -1,25 +1,24 @@
-import Head from 'next/head'
-import { useState, useEffect } from 'react';
+import Head from 'next/head';
+import { useState } from 'react';
+// import styles from '../styles/Home.module.scss';
 
 export default function Home() {
   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    fetch('http://localhost:3000/').then(r => r.json()).then(res => {
-      console.log(res);
-    });
-  }, []);
-
   return (
-    <div>
+    <div className="">
       <Head>
         <title>sei-whale</title>
       </Head>
       <h1>Sei Whale Service!!</h1>
       <div>{count}</div>
-      <button type="button" onClick={() => {
-        setCount(count + 1);
-      }}>증가</button>
+      <button
+        type="button"
+        onClick={() => {
+          setCount(count + 1);
+        }}>
+        증가
+      </button>
     </div>
-  )
+  );
 }
