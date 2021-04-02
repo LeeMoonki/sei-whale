b6363153fb9c0d2c151f4bc375568aeee1ba33ee
Moonki Lee <shinbaek89@gmail.com>
2021-04-02 22:28:00 +0900

    new: husky 세팅

--- services/webapp/pages/index.tsx
+++ services/webapp/pages/index.tsx
@@ -16,7 +16,8 @@ export default function Home() {
         type="button"
         onClick={() => {
           setCount(count + 1);
-        }}>
+        }}
+      >
         증가
       </button>
     </div>
