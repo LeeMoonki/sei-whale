0daefa243cf784cb0da3f891478415cdabe5a713
Moonki Lee <shinbaek89@gmail.com>
2021-04-17 09:30:19 +0900

    new: server에 eslint, prettier 추가

---
+++ services/server/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVerseion: 8
+  },
+  env: {
+    node: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ]
+};
