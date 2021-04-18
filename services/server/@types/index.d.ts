c8da6afea1252efd29910b81064b09ffa5ae1ab3
Moonki Lee <shinbaek89@gmail.com>
2021-04-19 08:33:28 +0900

    up: FastifyInstance 병합 타입 수정

--- services/server/@types/index.d.ts
+++ services/server/@types/index.d.ts
@@ -1,12 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import fastify from 'fastify';
+import fastify, { FastifyLoggerInstance } from 'fastify';
 import { IUserApplicationService } from '../src/services/app/UserApplicationService';
 
 declare module 'fastify' {
   export interface FastifyInstance<
     HttpServer = Server,
     HttpRequest = IncomingMessage,
-    HttpResponse = ServerResponse
+    HttpResponse = ServerResponse,
+    Logger = FastifyLoggerInstance
   > {
     services: {
       user: IUserApplicationService;
