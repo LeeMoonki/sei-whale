59b0e0c34c0c7bb78894a7c8b717d5ec08a91480
Junep <prepared@inflearn.com>
2021-04-23 09:51:15 +0900

    new: login 페이지

---
+++ services/webapp/pages/login.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head';
+import { useState } from 'react';
+import styles from '../styles/Home.module.scss';
+
+export default function Home() {
+  const [email, setEmail] = useState('foo');
+  const [password, setPassword] = useState('bar');
+
+  const submit = () => {
+    const body = { email, password };
+
+    fetch('http://localhost:7070/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(body),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        console.log('result : ', result);
+      });
+  };
+
+  return (
+    <div id={styles.app}>
+      <Head>
+        <title>sei-whale | 로그인</title>
+      </Head>
+      <form onSubmit={(e) => e.preventDefault()}>
+        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} />
+        <input type="password" onChange={(e) => setPassword(e.target.value)} />
+        <button onClick={submit}>로그인</button>
+      </form>
+    </div>
+  );
+}
