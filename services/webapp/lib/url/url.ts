a5218b3e4c9b28e43f3f2741e8751546db54dee7
Moonki Lee <shinbaek89@gmail.com>
2021-05-05 21:13:18 +0900

    chore: url 복구

--- services/webapp/lib/url/url.ts
+++ services/webapp/lib/url/url.ts
@@ -1,7 +1,6 @@
-// import { DOMAIN } from '../../config';
-// import { getQueryString, QueryStrings } from './queryString';
+import { DOMAIN } from '../../config';
+import { getQueryString, QueryStrings } from './queryString';
 
-export function url(/*path?: string, queryStringObject?: QueryStrings*/) {
-  // return (path ? (DOMAIN as string) + path : DOMAIN) + getQueryString(queryStringObject);
-  return '';
+export function url(path?: string, queryStringObject?: QueryStrings) {
+  return (path ? (DOMAIN as string) + path : DOMAIN) + getQueryString(queryStringObject);
 }
