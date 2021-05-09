9d4fd0f2ee22c8640d9741f55f04941cd380f09a
Moonki Lee <shinbaek89@gmail.com>
2021-05-09 16:02:31 +0900

    up: server의 cookie 테스트

--- services/server/__tests__/lib/cookie.test.ts
+++ services/server/__tests__/lib/cookie.test.ts
@@ -1,5 +1,27 @@
-import { parse } from '../../src/lib/cookie';
+import * as cookie from '../../src/lib/cookie';
 
-test('parse', () => {
-  expect(parse('a=10;b=20')).toEqual({ a: '10', b: '20' });
+test('convertStringToProperValue', () => {
+  const assertionSet = [
+    { received: '10', expected: 10 },
+    { received: 'NaN', expected: NaN },
+    { received: 'true', expected: true },
+    { received: 'false', expected: false },
+    { received: 'null', expected: null },
+    { received: 'undefined', expected: undefined },
+  ];
+
+  for (const a of assertionSet) {
+    expect(cookie.convertStringToProperValue(a.received)).toBe(a.expected);
+  }
+});
+
+test('parseCookieString', () => {
+  const cookieString = 'a=10;b=20;c=null;d=undefined';
+  const count = cookieString.split(';').length;
+
+  jest.spyOn(cookie, 'convertStringToProperValue');
+
+  cookie.parseCookieString(cookieString);
+
+  expect(cookie.convertStringToProperValue).toBeCalledTimes(count);
 });
--- services/server/src/lib/cookie.ts
+++ services/server/src/lib/cookie.ts
@@ -1,10 +1,30 @@
 /** cookie를 객체로 파싱 */
-export const parse = (cookie: string) => {
-  const result: { [key: string]: string } = {};
+export const convertStringToProperValue = (str: string) => {
+  const num = Number(str);
+  if (!isNaN(num)) {
+    return num;
+  } else {
+    switch (str) {
+      case 'true':
+        return true;
+      case 'false':
+        return false;
+      case 'null':
+        return null;
+      case 'undefined':
+        return undefined;
+    }
+
+    return NaN;
+  }
+};
+
+export const parseCookieString = (cookie: string) => {
+  const result: { [key: string]: any } = {};
   cookie
     .split(';')
     .map((kv) => kv.trim().split('='))
-    .forEach(([k, v]) => (result[k] = v));
+    .forEach(([k, v]) => (result[k] = convertStringToProperValue(v)));
 
   return result;
 };
